eca603f5fefaa667b2db1606b05d504427949bf8
Yahyo <mirxodjaevyahyo@gmail.com>
2025-05-04 12:12:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let a = prompt('What is your name?')
+let b = prompt('What is your second name?')
+var c = prompt('How old are you?')
+let d = prompt('Where is your hometown?')
+let e = prompt(`Write your e-mail`)
+
+console.log(` your name: ${a} \n your second name: ${b} \n your hometown: ${d} \n your age: ${c} \n your e-mail: ${e}`);
+
+
